7f613b9a727b5f7a79f661d6190cba03459e20c7
MihailoPesic <pesic.mihailo@jjzmaj.edu.rs>
2024-07-25 13:43:05 +0200

    primenjeno odredjivanje uz pomoc funkcije iz simulacije. Nakon ovoga sledi merenje.

--- main.py
+++ main.py
@@ -6,27 +6,46 @@ import traceback
 import numpy as np
 import math
 
-def delay_and_sum(signal, angle_grid, sample_rate, num_samples, d):
+# def delay_and_sum(signal, angle_grid, sample_rate, num_samples, d):
+#     u = 343 # [m/s]
+#     dist_per_sample = u/sample_rate
+#     count = int((10*d) / dist_per_sample)
+#     _max = -100000000
+#     tdoa = 0
+#     for i in range(count):
+#         temp1 = np.dot(signal[0], np.concatenate((np.roll(signal[1], i)[i:], np.zeros(i,dtype = int))))
+#         if i == 0:
+#             temp2 = -1000000
+#         else:
+#             temp2 = np.dot(signal[0], np.concatenate((np.zeros(i,dtype = int), np.roll(signal[1], -i)[:-i])))
+        
+#         if temp1 > _max:
+#             _max = temp1
+#             tdoa = i
+#         if temp2 > _max:
+#             _max = temp1
+#             tdoa = -i
+#     tdoa = tdoa/sample_rate
+#     power = 180*math.acos((u*tdoa)/d)/math.pi
+#     return power
+
+def delay_and_sum(signal, sample_rate, num_samples, d):
     u = 343 # [m/s]
     dist_per_sample = u/sample_rate
-    count = int((10*d) / dist_per_sample)
-    _max = -100000000
+    count = int((3*d) / dist_per_sample)
+    _max = 1e-12
     tdoa = 0
-    for i in range(count):
-        temp1 = np.dot(signal[0], np.concatenate((np.roll(signal[1], i)[i:], np.zeros(i,dtype = int))))
-        if i == 0:
-            temp2 = -1000000
-        else:
-            temp2 = np.dot(signal[0], np.concatenate((np.zeros(i,dtype = int), np.roll(signal[1], -i)[:-i])))
-        
-        if temp1 > _max:
+    t1_list = []
+    t2_list = []
+    for i in range(-count, count):
+        temp1 = np.dot(signal[0], np.roll(signal[1], i))#np.concatenate((np.roll(signal[1], i)[i:], np.zeros(i,dtype = int))))
+        if temp1 > _max and -1 <= (u*(i/sample_rate))/d <=1:
             _max = temp1
             tdoa = i
-        if temp2 > _max:
-            _max = temp1
-            tdoa = -i
+        t1_list.append(temp1)
+
     tdoa = tdoa/sample_rate
-    power = 180*math.acos((u*tdoa)/d)/math.pi-90
+    power = 180*np.acos((u*tdoa)/d)/math.pi
     return power
 
 x_data, y_data = [], []
@@ -35,7 +54,7 @@ speed_of_sound = 343  # Speed of sound in m/s
 sample_rate = 24000 # Hz
 
 
-with serial.Serial('/dev/ttyACM0', 10000000, timeout=0.01) as ser:
+with serial.Serial('COM10', 10000000, timeout=0.01) as ser:
 
     x_data, y_data = [], []
 
@@ -58,7 +77,7 @@ with serial.Serial('/dev/ttyACM0', 10000000, timeout=0.01) as ser:
             ser.timeout = 0.2
             data = ser.read(16384 - 1)
 
-            data = data + c
+            data = c + data
 
             if len(data) < 16384:
                 return
@@ -78,11 +97,11 @@ with serial.Serial('/dev/ttyACM0', 10000000, timeout=0.01) as ser:
 
             signal = np.vstack((data1, data2))
             angle_grid = np.linspace(-90, 90, 181)
-            power = delay_and_sum(signal, angle_grid, sample_rate, num_samples, spacing)
+            power = delay_and_sum(signal, sample_rate, num_samples, spacing)
             print(power)
-            #line3.set_data(angle_grid, power)
-            #ax2.relim()
-            #ax2.autoscale_view()
+            # line3.set_data(angle_grid, power)
+            # ax2.relim()
+            # ax2.autoscale_view()
 
         except KeyboardInterrupt:
             exit()
